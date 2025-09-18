e36008195b750c2bd6c799ec70053f1f6ea57d24
gibbey234 <tyrellkwrafuse@gmail.com>
2025-09-18 15:32:37 -0300

    sdf

--- Week3/lab3_part2.py
+++ Week3/lab3_part2.py
@@ -1,6 +1,8 @@
 score = int(input("Enter your test score (0-100): "))
 
-if score >= 90:
+if score == 100:
+    print("Grade = Perect Score!")
+elif score >= 90:
     print("Grade = A")
 elif score >= 80:
     print("Grade = B")
